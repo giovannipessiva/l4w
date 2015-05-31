302cc7c62407f611b2b77b8ab0315b31edaebe04
Giovanni Pessiva <giovanni.pessiva@gmail.com>
2015-05-31 20:50:44 +0200

    Setup

--- server/index.js
+++ server/index.js
@@ -5,9 +5,9 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('It\'s not ready yet');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('L4W is running on port', app.get('port'));
 });
