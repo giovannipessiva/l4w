2977da18df038fe99b97323d4843a2dca8d20c42
Giovanni Pessiva <giovanni.pessiva@gmail.com>
2015-05-31 21:40:07 +0200

    Grunt integration

---
+++ GruntFile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'), 
+
+		concat: {
+			options: {
+				// define a string to put between each file in the concatenated output
+				separator: ';'
+			},
+			dist: {
+				// the files to concatenate
+				src: ['client/*/js/*.js'],
+				// the location of the resulting JS file
+				dest: 'client/l4w.js'
+			}
+		},
+
+		uglify: {
+			options: {
+				// the banner is inserted at the top of the output
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			dist: {
+				files: {
+				'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	grunt.registerTask('default', ['concat', 'uglify']);
+};
