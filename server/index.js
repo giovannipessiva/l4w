8ce35f6066de59f3bb5a4e77afe5f5b4e4c2f12f
Giovanni Pessiva <giovannipessiva@users.noreply.github.com>
2015-06-02 00:10:07 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/server'));
 
 app.get('/', function(request, response) {
   response.sendfile(__dirname + '/server/views/home.html');
