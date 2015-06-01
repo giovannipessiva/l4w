2b15c72cfffd8c4e0200fb9739a40cc7b1e3f0d7
Giovanni Pessiva <giovannipessiva@users.noreply.github.com>
2015-06-02 00:02:42 +0200

    Test view

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('It\'s not ready yet');
+  response.sendfile(__dirname + '/server/views/home.html');
 });
 
 app.listen(app.get('port'), function() {
