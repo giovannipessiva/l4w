7925d062680945e37c3ab2e6a0314768ac970915
Travis CI <giovannipessiva@users.noreply.github.com>
2015-06-25 19:37:53 +0200

    Module test

--- client/modules/InputHandler.js
+++ client/modules/InputHandler.js
@@ -1,60 +1,65 @@
-var Keys = (function () {
-    function Keys() {
+var Input;
+(function (Input) {
+    var Keys = (function () {
+        function Keys() {
+        }
+        Keys.UP = "38";
+        Keys.DOWN = "40";
+        Keys.LEFT = "37";
+        Keys.RIGHT = "39";
+        Keys.CTRL = "17";
+        Keys.ALT = "18";
+        Keys.ENTER = "13";
+        Keys.SPACE = "32";
+        Keys.CAPS = "20";
+        Keys.SHIFT = "16";
+        Keys.W = "87";
+        Keys.A = "65";
+        Keys.D = "68";
+        Keys.S = "83";
+        Keys.J = "74";
+        Keys.K = "75";
+        return Keys;
+    })();
+    Input.Keys = Keys;
+    ;
+    ;
+    function initInputHandler(canvas, inputCallbacks, resetCallback, pauseCallback, unpauseCallback) {
+        var lastKey;
+        var flagPause = false;
+        inputCallbacks[Keys.SPACE] = function () {
+            if (flagPause) {
+                unpauseCallback();
+                flagPause = false;
+            }
+            else {
+                pauseCallback();
+                flagPause = true;
+            }
+        };
+        document.addEventListener("keydown", function (e) {
+            var callback = inputCallbacks[String(e.keyCode)];
+            if (callback !== undefined) {
+                callback();
+            }
+            lastKey = e.keyCode;
+        });
+        document.addEventListener("keyup", function (e) {
+            if (e.keyCode === lastKey) {
+                resetCallback();
+            }
+        });
+        document.addEventListener("visibilitychange", function onVisibilityChange() {
+            if (document.hidden) {
+                pauseCallback();
+                flagPause = true;
+            }
+            else {
+                unpauseCallback();
+                flagPause = false;
+            }
+        }, false);
     }
-    Keys.UP = "38";
-    Keys.DOWN = "40";
-    Keys.LEFT = "37";
-    Keys.RIGHT = "39";
-    Keys.CTRL = "17";
-    Keys.ALT = "18";
-    Keys.ENTER = "13";
-    Keys.SPACE = "32";
-    Keys.CAPS = "20";
-    Keys.SHIFT = "16";
-    Keys.W = "87";
-    Keys.A = "65";
-    Keys.D = "68";
-    Keys.S = "83";
-    Keys.J = "74";
-    Keys.K = "75";
-    return Keys;
-})();
-;
-;
-function initInputHandler(canvas, inputCallbacks, resetCallback, pauseCallback, unpauseCallback) {
-    var lastKey;
-    var flagPause = false;
-    inputCallbacks[Keys.SPACE] = function () {
-        if (flagPause) {
-            unpauseCallback();
-            flagPause = false;
-        }
-        else {
-            pauseCallback();
-            flagPause = true;
-        }
-    };
-    document.addEventListener("keydown", function (e) {
-        var callback = inputCallbacks[String(e.keyCode)];
-        if (callback !== undefined) {
-            callback();
-        }
-        lastKey = e.keyCode;
-    });
-    document.addEventListener("keyup", function (e) {
-        if (e.keyCode === lastKey) {
-            resetCallback();
-        }
-    });
-    document.addEventListener("visibilitychange", function onVisibilityChange() {
-        if (document.hidden) {
-            pauseCallback();
-            flagPause = true;
-        }
-        else {
-            unpauseCallback();
-            flagPause = false;
-        }
-    }, false);
-}
-;
+    Input.initInputHandler = initInputHandler;
+    ;
+})(Input || (Input = {}));
--- client/modules/InputHandler.ts
+++ client/modules/InputHandler.ts
@@ -16,134 +16,136 @@ Usage example:
         function() { console.log("Unpaused"); }
     );
 */
+module Input {
 
-export class Keys {
-    static UP : string = "38";
-    static DOWN : string = "40";
-    static LEFT : string = "37";
-    static RIGHT : string = "39";
-    static CTRL : string = "17";
-    static ALT : string = "18";
-    static ENTER : string = "13";
-    static SPACE : string = "32";
-    static CAPS : string = "20";
-    static SHIFT : string = "16";
-    static W : string = "87";
-    static A : string = "65";
-    static D : string = "68";
-    static S : string = "83";
-    static J : string = "74";
-    static K : string = "75";
-}
-
-interface IKeyCallback { (key: number) : void };
-interface IEventCallback { () : void };
-
-export function initInputHandler(
-    canvas : HTMLCanvasElement,
-    inputCallbacks : {Keys : IKeyCallback},
-    resetCallback : IEventCallback,
-    pauseCallback : IEventCallback,
-    unpauseCallback : IEventCallback) {
-	
-	var lastKey : number;
-    var flagPause : boolean = false;
+    export class Keys {
+        static UP : string = "38";
+        static DOWN : string = "40";
+        static LEFT : string = "37";
+        static RIGHT : string = "39";
+        static CTRL : string = "17";
+        static ALT : string = "18";
+        static ENTER : string = "13";
+        static SPACE : string = "32";
+        static CAPS : string = "20";
+        static SHIFT : string = "16";
+        static W : string = "87";
+        static A : string = "65";
+        static D : string = "68";
+        static S : string = "83";
+        static J : string = "74";
+        static K : string = "75";
+    }
     
-    // Always use SPACE for pause
-    inputCallbacks[Keys.SPACE] = function() {
-       if(flagPause) {
-           unpauseCallback();
-           flagPause = false;
-       } else {
-           pauseCallback();
-           flagPause = true;
-       }
+    interface IKeyCallback { (key: number) : void };
+    interface IEventCallback { () : void };
+    
+    export function initInputHandler(
+        canvas : HTMLCanvasElement,
+        inputCallbacks : {Keys : IKeyCallback},
+        resetCallback : IEventCallback,
+        pauseCallback : IEventCallback,
+        unpauseCallback : IEventCallback) {
+    	
+    	var lastKey : number;
+        var flagPause : boolean = false;
+        
+        // Always use SPACE for pause
+        inputCallbacks[Keys.SPACE] = function() {
+           if(flagPause) {
+               unpauseCallback();
+               flagPause = false;
+           } else {
+               pauseCallback();
+               flagPause = true;
+           }
+        };
+        
+    	
+        document.addEventListener("keydown", function(e) {
+            var callback = inputCallbacks[String(e.keyCode)];
+            if(callback !== undefined) {
+                callback();
+            }
+            lastKey = e.keyCode;
+        });
+        document.addEventListener("keyup", function(e){
+            if(e.keyCode === lastKey) {
+                resetCallback();
+            }
+        });
+        
+    	document.addEventListener("visibilitychange", function onVisibilityChange(){
+    		if(document.hidden){
+                pauseCallback();
+                flagPause = true;
+    		}else{
+                unpauseCallback();
+                flagPause = false;
+    		} 
+    	}, false);
+    	
+    //	S.c2.addEventListener("mousemove", function(evt){
+    //		var rect = S.c2.getBoundingClientRect();
+    //        mouse_x = evt.clientX - rect.left;
+    //        mouse_y = evt.clientY - rect.top;
+    //    }, false);
+    //	S.c2.addEventListener("click", function(ect){
+    //		usingMouse = true;
+    //		var mouse_i = Math.floor((mouse_x - S.abs_x) / CELL_W);
+    //		var mouse_j = Math.floor((mouse_y - S.abs_y) / CELL_H);
+    //		clickOrTouch(mouse_i,mouse_j);
+    //	}, false);
+    //
+    //	S.c2.addEventListener("touchstart", function(evt){
+    //		usingMouse = false;
+    //		evt.preventDefault();
+    //		var rect = S.c2.getBoundingClientRect();
+    //		touch_x = event.targetTouches[0].pageX - rect.left;
+    //        touch_y = event.targetTouches[0].pageY - rect.top;
+    //		var touch_i = Math.floor((touch_x - S.abs_x) / CELL_W);
+    //		var touch_j = Math.floor((touch_y - S.abs_y) / CELL_H);
+    //		clickOrTouch(touch_i,touch_j);
+    //    }, false);
+    //	S.c2.addEventListener("touchmove", function(evt){
+    //		evt.preventDefault();
+    //		var rect = S.c2.getBoundingClientRect();
+    //		touch_x = event.targetTouches[0].pageX - rect.left;
+    //        touch_y = event.targetTouches[0].pageY - rect.top;
+    //		var touch_i = Math.floor((touch_x - S.abs_x) / CELL_W);
+    //		var touch_j = Math.floor((touch_y - S.abs_y) / CELL_H);
+    //		clickOrTouch(touch_i,touch_j);
+    //	}, false);
+    //	
+    //	
+    //	
+    //	function clickOrTouch(i,j){
+    //		var eventSelected = false;
+    //		for(var id in E){
+    //			var active_page = E[id][ACTIVE];
+    //			var e = E[id][active_page];
+    //			
+    //			var e_i = Math.floor(e.current_x/CELL_W);
+    //			var e_j = Math.floor(e.current_y/CELL_H);
+    //			
+    //			if(e_i==i && e_j==j){
+    //				eventSelected = true;
+    //				if(e == H){
+    //					//TODO apri l'inventario
+    //					console.log("open inventory");
+    //				
+    //				}else{
+    //					//TODO valuta evento
+    //					H.moveTo(i,j,true,function(){});
+    //				}
+    //			}
+    //		}
+    //		if(eventSelected == false){
+    //			//Move hero to this coordinates
+    //			H.moveTo(i,j,true,function(){});
+    //		}
+    //	}
+    	
+    	
     };
-    
-	
-    document.addEventListener("keydown", function(e) {
-        var callback = inputCallbacks[String(e.keyCode)];
-        if(callback !== undefined) {
-            callback();
-        }
-        lastKey = e.keyCode;
-    });
-    document.addEventListener("keyup", function(e){
-        if(e.keyCode === lastKey) {
-            resetCallback();
-        }
-    });
-    
-	document.addEventListener("visibilitychange", function onVisibilityChange(){
-		if(document.hidden){
-            pauseCallback();
-            flagPause = true;
-		}else{
-            unpauseCallback();
-            flagPause = false;
-		} 
-	}, false);
-	
-//	S.c2.addEventListener("mousemove", function(evt){
-//		var rect = S.c2.getBoundingClientRect();
-//        mouse_x = evt.clientX - rect.left;
-//        mouse_y = evt.clientY - rect.top;
-//    }, false);
-//	S.c2.addEventListener("click", function(ect){
-//		usingMouse = true;
-//		var mouse_i = Math.floor((mouse_x - S.abs_x) / CELL_W);
-//		var mouse_j = Math.floor((mouse_y - S.abs_y) / CELL_H);
-//		clickOrTouch(mouse_i,mouse_j);
-//	}, false);
-//
-//	S.c2.addEventListener("touchstart", function(evt){
-//		usingMouse = false;
-//		evt.preventDefault();
-//		var rect = S.c2.getBoundingClientRect();
-//		touch_x = event.targetTouches[0].pageX - rect.left;
-//        touch_y = event.targetTouches[0].pageY - rect.top;
-//		var touch_i = Math.floor((touch_x - S.abs_x) / CELL_W);
-//		var touch_j = Math.floor((touch_y - S.abs_y) / CELL_H);
-//		clickOrTouch(touch_i,touch_j);
-//    }, false);
-//	S.c2.addEventListener("touchmove", function(evt){
-//		evt.preventDefault();
-//		var rect = S.c2.getBoundingClientRect();
-//		touch_x = event.targetTouches[0].pageX - rect.left;
-//        touch_y = event.targetTouches[0].pageY - rect.top;
-//		var touch_i = Math.floor((touch_x - S.abs_x) / CELL_W);
-//		var touch_j = Math.floor((touch_y - S.abs_y) / CELL_H);
-//		clickOrTouch(touch_i,touch_j);
-//	}, false);
-//	
-//	
-//	
-//	function clickOrTouch(i,j){
-//		var eventSelected = false;
-//		for(var id in E){
-//			var active_page = E[id][ACTIVE];
-//			var e = E[id][active_page];
-//			
-//			var e_i = Math.floor(e.current_x/CELL_W);
-//			var e_j = Math.floor(e.current_y/CELL_H);
-//			
-//			if(e_i==i && e_j==j){
-//				eventSelected = true;
-//				if(e == H){
-//					//TODO apri l'inventario
-//					console.log("open inventory");
-//				
-//				}else{
-//					//TODO valuta evento
-//					H.moveTo(i,j,true,function(){});
-//				}
-//			}
-//		}
-//		if(eventSelected == false){
-//			//Move hero to this coordinates
-//			H.moveTo(i,j,true,function(){});
-//		}
-//	}
-	
-	
-};
+}
