6fda15a08072c8018c0b1162a17abd9ff80c0d13
g.pessiva <g.pessiva@Blu03583.mshome.net>
2015-06-26 18:23:49 +0200

    Main module

---
+++ client/modules/Main.js
@@ -0,0 +1,12 @@
+var canvas = document.getElementById('canvas1');
+Main.start(canvas);
+var Main;
+(function (Main) {
+    function start(canvas) {
+        initInput(canvas);
+    }
+    Main.start = start;
+    function initInput(canvas) {
+    }
+    ;
+})(Main || (Main = {}));
---
+++ client/modules/Main.ts
@@ -0,0 +1,36 @@
+/// <reference path="Input.ts" />
+
+var canvas: HTMLCanvasElement = <HTMLCanvasElement> document.getElementById('canvas1');
+Main.start(canvas);
+
+module Main {
+   
+    export function start(canvas) {
+        initInput(canvas);
+    }
+
+    function initInput(canvas) {
+        /*
+        var inputCallbackMap: { Keys: IKeyCallback; } = {};
+        inputCallbackMap[Input.Keys.UP] = function(key) { alert("Up"); };
+        inputCallbackMap[Input.Keys.DOWN] = function(key) { alert("Down"); };
+        inputCallbackMap[Input.Keys.LEFT] = function(key) { alert("Left"); };
+        inputCallbackMap[Input.Keys.RIGHT] = function(key) { alert("Right"); };
+
+        Input.handleInput(
+            canvas,
+            function() { },
+            function() { },
+            function() { },
+            function() { },
+            function() { },
+            inputCallbackMap,
+            function() { },
+            function() { },
+            function() { },
+            function() { }
+        );
+        */
+    };
+    
+}
