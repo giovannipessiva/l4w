b9f0154657d3d7dcd056b060070cc5ebdbce6dc2
rpt <rpt@altervista.org>
2015-06-27 10:16:13 +0200

    Input test configuration

--- client/modules/Main.js
+++ client/modules/Main.js
@@ -5,6 +5,38 @@ var Main;
     }
     Main.start = start;
     function initInput(canvas) {
+        var inputCallbackMap;
+        inputCallbackMap[Input.Keys.UP] = function (key) {
+            console.log("Up");
+        };
+        inputCallbackMap[Input.Keys.DOWN] = function (key) {
+            console.log("Down");
+        };
+        inputCallbackMap[Input.Keys.LEFT] = function (key) {
+            console.log("Left");
+        };
+        inputCallbackMap[Input.Keys.RIGHT] = function (key) {
+            console.log("Right");
+        };
+        Input.handleInput(canvas, function () {
+            console.log("action");
+        }, function () {
+            console.log("start");
+        }, function () {
+            console.log("ongoing");
+        }, function () {
+            console.log("end");
+        }, function () {
+            console.log("hover");
+        }, inputCallbackMap, function () {
+            console.log("reset");
+        }, function () {
+            console.log("pause");
+        }, function () {
+            console.log("unpause");
+        }, function () {
+            console.log("resize");
+        });
     }
     ;
 })(Main || (Main = {}));
--- client/modules/Main.ts
+++ client/modules/Main.ts
@@ -7,27 +7,25 @@ module Main {
     }
 
     function initInput(canvas) {
-        /*
-        var inputCallbackMap: { Keys: IKeyCallback; } = {};
-        inputCallbackMap[Input.Keys.UP] = function(key) { alert("Up"); };
-        inputCallbackMap[Input.Keys.DOWN] = function(key) { alert("Down"); };
-        inputCallbackMap[Input.Keys.LEFT] = function(key) { alert("Left"); };
-        inputCallbackMap[Input.Keys.RIGHT] = function(key) { alert("Right"); };
+        var inputCallbackMap: { Keys: Input.IKeyCallback; };
+        inputCallbackMap[Input.Keys.UP] = function(key) { console.log("Up"); };
+        inputCallbackMap[Input.Keys.DOWN] = function(key) { console.log("Down"); };
+        inputCallbackMap[Input.Keys.LEFT] = function(key) { console.log("Left"); };
+        inputCallbackMap[Input.Keys.RIGHT] = function(key) { console.log("Right"); };
 
         Input.handleInput(
             canvas,
-            function() { },
-            function() { },
-            function() { },
-            function() { },
-            function() { },
+            function() { console.log("action"); },
+            function() { console.log("start"); },
+            function() { console.log("ongoing"); },
+            function() { console.log("end"); },
+            function() { console.log("hover"); },
             inputCallbackMap,
-            function() { },
-            function() { },
-            function() { },
-            function() { }
+            function() { console.log("reset"); },
+            function() { console.log("pause"); },
+            function() { console.log("unpause"); },
+            function() { console.log("resize"); }
         );
-        */
     };
     
 }
