2bc8511a55ab7151256a4278dde29449fde2880b
rpt <rpt@altervista.org>
2015-07-05 19:54:57 +0200

    Server resources management

--- server/index.js
+++ server/index.js
@@ -2,12 +2,26 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/server'));
 
 app.get('/', function(request, response) {
-  response.sendfile(__dirname + '/views/home.html');
+	response.sendfile(__dirname + '/server/views/home.html');
+});
+
+app.get('/js/:script', function(request, response) {
+	var script = req.params.script;
+	response.sendfile(__dirname + '/client/module/' + script);
+});
+
+app.get('/data/:file', function(request, response) {
+	var file = req.params.file;
+	response.sendfile(__dirname + '/client/data/' + file);
+});
+
+app.get('/assets/:file', function(request, response) {
+	var file = req.params.file;
+	response.sendfile(__dirname + '/client/assets/' + file);
 });
 
 app.listen(app.get('port'), function() {
-  console.log('L4W is running on port', app.get('port'));
+	console.log('L4W is running on port', app.get('port'));
 });
