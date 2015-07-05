56105fda4f2982189004cb231e02f5988fe4a6f4
rpt <rpt@altervista.org>
2015-07-05 20:09:48 +0200

    Fix filepath resolving

--- server/index.js
+++ server/index.js
@@ -1,25 +1,29 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-	response.sendfile(__dirname + 'views/home.html');
+	response.sendFile(__dirname + '/views/home.html');
 });
 
 app.get('/js/:script', function(request, response) {
 	var script = request.params.script;
-	response.sendfile(__dirname + '../client/module/' + script);
+	var filePath = path.resolve(__dirname + '/../client/module');
+	response.sendFile(filePath + "/" + script);
 });
 
 app.get('/data/:file', function(request, response) {
 	var file = request.params.file;
-	response.sendfile(__dirname + '../client/data/' + file);
+	var filePath = path.resolve(__dirname + '/../client/data');
+	response.sendFile(filePath + "/" + file);
 });
 
 app.get('/assets/:file', function(request, response) {
 	var file = request.params.file;
-	response.sendfile(__dirname + '../client/assets/' + file);
+	var filePath = path.resolve(__dirname + '/../client/assets');
+	response.sendFile(filePath + "/" + file);
 });
 
 app.listen(app.get('port'), function() {
