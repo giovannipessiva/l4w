65fb6e78cb79e58aa64f40c0c084fe901abeb831
rpt <rpt@altervista.org>
2015-07-05 20:00:36 +0200

    Fix routing

--- server/index.js
+++ server/index.js
@@ -4,22 +4,22 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-	response.sendfile(__dirname + '/server/views/home.html');
+	response.sendfile(__dirname + 'views/home.html');
 });
 
 app.get('/js/:script', function(request, response) {
-	var script = req.params.script;
-	response.sendfile(__dirname + '/client/module/' + script);
+	var script = request.params.script;
+	response.sendfile(__dirname + '../client/module/' + script);
 });
 
 app.get('/data/:file', function(request, response) {
-	var file = req.params.file;
-	response.sendfile(__dirname + '/client/data/' + file);
+	var file = request.params.file;
+	response.sendfile(__dirname + '../client/data/' + file);
 });
 
 app.get('/assets/:file', function(request, response) {
-	var file = req.params.file;
-	response.sendfile(__dirname + '/client/assets/' + file);
+	var file = request.params.file;
+	response.sendfile(__dirname + '../client/assets/' + file);
 });
 
 app.listen(app.get('port'), function() {
