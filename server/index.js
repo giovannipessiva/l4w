202953d463302696001fb377bc57f7477e892a19
rpt <rpt@altervista.org>
2016-01-01 23:04:49 +0100

    Postgres integration

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ var path = require('path');
 var express = require('express');
 var favicon = require('serve-favicon');
 var compression = require('compression');
+var pg = require('pg');
 
 var mapper = require(__dirname + '/modules/mapper');
 var utils = require(__dirname + '/modules/utils');
@@ -69,6 +70,19 @@ app.post('/edit/maps', function(request, response) {
 	request.on('data', function(body) {
 		mapper.updateMaps(body, response);
 	});
+});
+
+//Test connection
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+	if (err)
+		throw err;
+	console.log('Connected to postgres! Getting schemas...');
+
+	client.query(
+			'SELECT table_schema,table_name FROM information_schema.tables;')
+			.on('row', function(row) {
+				console.log(JSON.stringify(row));
+			});
 });
 
 // Startup message
