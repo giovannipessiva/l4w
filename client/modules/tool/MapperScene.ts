08bfed025c5708620a3252cef638f9afa4d38298
rpt <rpt@altervista.org>
2016-08-10 12:48:46 +0200

    Fix esare without selection

--- client/modules/tool/MapperScene.ts
+++ client/modules/tool/MapperScene.ts
@@ -49,42 +49,48 @@ class MapperScene extends StaticScene {
     apply(x: number, y: number): boolean {
         var changed: boolean = false;
         var pickerArea: IRectangle = this.tilePicker.getSelectionArea();
-        if (!Utils.isEmpty(pickerArea)) {
-            var changedCell: number = x + y * this.map.width;
-            if (Utils.isEmpty(this.map.layers[this.activeLayer].data)) {
-                this.map.layers[this.activeLayer].data = [];
-            }
-            switch (this.editMode) {
-                case Constant.EditMode.APPLY:
-                    var tileColumns: number = this.map.tileset.imagewidth / this.grid.cellW; //TODO questa non cambia mai, ottimizzabile
-                    var appliedTile: number = pickerArea.x1 + pickerArea.y1 * tileColumns;
-                    for (var j = 0; j <= pickerArea.y2 - pickerArea.y1; j++) {
-                        for (var i = 0; i <= pickerArea.x2 - pickerArea.x1; i++) {
-                            if (x + i < this.map.width) {
-                                var appliedTileOffset: number = i + j * tileColumns;
-                                var changedCellOffset: number = i + j * this.map.width;
-                                if(this.map.layers[this.activeLayer].data[changedCell + changedCellOffset] !== appliedTile + appliedTileOffset) {
-                                    changed = true;
-                                    this.map.layers[this.activeLayer].data[changedCell + changedCellOffset] = appliedTile + appliedTileOffset;
-                                }
+        var changedCell: number = x + y * this.map.width;
+        if (Utils.isEmpty(this.map.layers[this.activeLayer].data)) {
+            this.map.layers[this.activeLayer].data = [];
+        }
+        switch (this.editMode) {
+            case Constant.EditMode.APPLY:
+                if (Utils.isEmpty(pickerArea)) {
+                    return false;    
+                }
+                var tileColumns: number = this.map.tileset.imagewidth / this.grid.cellW; //TODO questa non cambia mai, ottimizzabile
+                var appliedTile: number = pickerArea.x1 + pickerArea.y1 * tileColumns;
+                for (var j = 0; j <= pickerArea.y2 - pickerArea.y1; j++) {
+                    for (var i = 0; i <= pickerArea.x2 - pickerArea.x1; i++) {
+                        if (x + i < this.map.width) {
+                            var appliedTileOffset: number = i + j * tileColumns;
+                            var changedCellOffset: number = i + j * this.map.width;
+                            if(this.map.layers[this.activeLayer].data[changedCell + changedCellOffset] !== appliedTile + appliedTileOffset) {
+                                changed = true;
+                                this.map.layers[this.activeLayer].data[changedCell + changedCellOffset] = appliedTile + appliedTileOffset;
                             }
                         }
                     }
-                    break;
-                case Constant.EditMode.ERASE:
-                    for (var j = 0; j <= pickerArea.y2 - pickerArea.y1; j++) {
-                        for (var i = 0; i <= pickerArea.x2 - pickerArea.x1; i++) {
-                            if (x + i < this.map.width) {
-                                var changedCellOffset: number = i + j * this.map.width;
-                                if (this.map.layers[this.activeLayer].data[changedCell + changedCellOffset] !== null) {
-                                    changed = true;
-                                    this.map.layers[this.activeLayer].data[changedCell + changedCellOffset] = null;
-                                }
+                }
+                break;
+            case Constant.EditMode.ERASE:
+                if (Utils.isEmpty(pickerArea)) {
+                    pickerArea = {
+                        x1:0, x2: 0, y1: 0, y2:0
+                    }
+                }
+                for (var j = 0; j <= pickerArea.y2 - pickerArea.y1; j++) {
+                    for (var i = 0; i <= pickerArea.x2 - pickerArea.x1; i++) {
+                        if (x + i < this.map.width) {
+                            var changedCellOffset: number = i + j * this.map.width;
+                            if (this.map.layers[this.activeLayer].data[changedCell + changedCellOffset] !== null) {
+                                changed = true;
+                                this.map.layers[this.activeLayer].data[changedCell + changedCellOffset] = null;
                             }
                         }
                     }
-                    break;
-            }
+                }
+                break;
         }
         //TODO gestisci trascinamento del picker
         return changed;
