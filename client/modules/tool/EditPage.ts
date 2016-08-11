4d991912ff847ff1ab10894d562dbd8ddbb1b991
rpt <rpt@altervista.org>
2016-08-11 23:44:56 +0200

    Fix delete

--- client/modules/tool/EditPage.ts
+++ client/modules/tool/EditPage.ts
@@ -4,11 +4,11 @@
 /// <reference path="../core/util/Commons.ts" />
 
 namespace EditPage {
-    
+
     export const PAGE_TITLE = document.title;
     export const BUTTON_ID_MODE = "mode";
     export const BUTTON_ID_LAYER = "layer";
-    
+
     let flagFirstLoad: boolean = true;
     let flagEdited: boolean = false;
 
@@ -28,39 +28,52 @@ namespace EditPage {
                 "dots": false
             }
         });
-        
-        var canvas = <HTMLCanvasElement> document.getElementById("canvas1");
+
+        var canvas = <HTMLCanvasElement>document.getElementById("canvas1");
 
         $("#mapPanel").on("changed.jstree", function(e, data) {
-            // Prevent double call at start
-            if (data.action === "ready") {
-                return;
+
+            switch (data.action) {
+                case "ready":
+                    // Prevent double call at start
+                    return;
+                case "delete_node":
+                    // Select another node
+                    let previousNode = $('#mapPanel').jstree(true).get_prev_dom(data.node);
+                    $('#mapPanel').jstree(true).select_node(previousNode);
+                    return;
+                case "model":
+                case "select_node":
+                    if (flagFirstLoad) {
+                        flagFirstLoad = false;
+                    }
+                    if (flagEdited) {
+                        alert("Prima di cambiare mappa e' necessario salvare");
+                        return;
+                    }
+                    $("#mapDetailPanel").show();
+                    var node: JSTreeNode = getSelectedNode();
+                    if (Utils.isEmpty(node.data)) {
+                        //TODO l'inizilizzazione va fatta da un'altra parte
+                        node.data = {
+                            w: 25,
+                            h: 20,
+                            tile: "002-Woods01.png"
+                        };
+                    }
+                    $("#mapSizeW").val(node.data.w + "");
+                    $("#mapSizeH").val(node.data.h + "");
+                    $("#tiles").val(node.data.tile);
+                    TilePicker.loadTile(node.data.tile, function(tilePicker: TilePickerScene) {
+                        Mapper.start(canvas, tilePicker, node.id);
+                    });
+                    break;
+                default:
+                    console.log("Action: " + data.action);
+                    break;
             }
-            if(flagFirstLoad) {
-                flagFirstLoad = false;
-            }
-            if(flagEdited) {
-                alert("Prima di cambiare mappa e' necessario salvare");
-                return;
-            }
-            $("#mapDetailPanel").show();
-            var node: JSTreeNode = getSelectedNode();
-            if(Utils.isEmpty(node.data)) {
-                //TODO l'inizilizzazione va fatta da un'altra parte
-                node.data = {
-                    w: 25,
-                    h: 20,
-                    tile: "002-Woods01.png"  
-                };
-            }
-            $("#mapSizeW").val(node.data.w + "");
-            $("#mapSizeH").val(node.data.h + "");
-            $("#tiles").val(node.data.tile);
-            TilePicker.loadTile(node.data.tile, function(tilePicker: TilePickerScene) {
-                Mapper.start(canvas, tilePicker, node.id);
-            });
         });
-        
+
         // Resize the panel to match the tileset
         var resizerCallback: IPropertiesCallback = function(props: Map<string, number>) {
             var width = +props.get("cellWidth") * +props.get("tileColumns") + 2;
@@ -69,7 +82,7 @@ namespace EditPage {
         Resource.loadProperties(resizerCallback);
 
         loadTiles();
-        
+
         loadNews();
     }
 
@@ -80,7 +93,7 @@ namespace EditPage {
         Mapper.changeSize(node.data.h, node.data.w);
         changeEditState(true);
     }
-    
+
     function loadTiles() {
         $.getJSON("data/resources/tiles.json", function(data) {
             var sel = $("#tiles");
@@ -90,7 +103,7 @@ namespace EditPage {
             }
         });
     }
-    
+
     export function loadNews() {
         $.getJSON("news", function(data) {
             var news = $("#news");
@@ -101,18 +114,18 @@ namespace EditPage {
     export function changeTile() {
         var node = $("#mapPanel").jstree(true).get_selected(true)[0];
         node.data.tile = $("#tiles").val();
-        TilePicker.loadTile(node.data.tile,function(tilePicker: TilePickerScene) {
+        TilePicker.loadTile(node.data.tile, function(tilePicker: TilePickerScene) {
             Mapper.changeTile(node.data.tile, tilePicker);
         });
         changeEditState(true);
     }
-    
+
     export function save() {
         Mapper.saveMap(function(result1) {
-            if(result1) {
+            if (result1) {
                 EditPage.changeEditState(false);
                 TilePicker.saveData(function(result2) {
-                    if(!result2) {
+                    if (!result2) {
                         console.error("Salvataggio fallito");
                     }
                 });
@@ -121,30 +134,31 @@ namespace EditPage {
             }
         });
     }
-            
+
     export function reload() {
         Mapper.reloadMap();
         //TODO reload tree
+        $('#mapPanel').jstree(true).refresh(false, false); //FIXME testare
     }
-    
-    export function getActiveMap() : string {
+
+    export function getActiveMap(): string {
         return getSelectedNode().id;
     }
 
     function getSelectedNode(): JSTreeNode {
         return $("#mapPanel").jstree(true).get_selected(true)[0];
     }
-    
+
     export function changeEditState(edited: boolean) {
         flagEdited = edited;
-        if(edited) {
+        if (edited) {
             document.title = PAGE_TITLE + "*";
         } else {
             document.title = PAGE_TITLE;
         }
-        (<HTMLButtonElement>$("#saveButton")[0]).disabled=!edited;
-        (<HTMLButtonElement>$("#reloadButton")[0]).disabled=!edited;
-        
+        (<HTMLButtonElement>$("#saveButton")[0]).disabled = !edited;
+        (<HTMLButtonElement>$("#reloadButton")[0]).disabled = !edited;
+
         // Disable maps selection
         var test = $("#mapPanel").jstree(true).get_json("#", {
             "flat": true,
@@ -153,9 +167,9 @@ namespace EditPage {
             "no_children": false,
             "no_data": false
         });
-        $.each(test, function( key: string, node: JSTreeNode ) {
+        $.each(test, function(key: string, node: JSTreeNode) {
             if (node.state.selected === false) {
-                if(edited) {
+                if (edited) {
                     $("#mapPanel").jstree("disable_node", node.id);
                 } else {
                     $("#mapPanel").jstree("enable_node", node.id);
