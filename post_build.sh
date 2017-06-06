2b5977bd9f41b6e6640a37c75272f2b7c0231781
rpt <giovannipessiva@users.noreply.github.com>
2017-06-06 21:15:05 +0200

    Your efforts mean nothing to me

--- post_build.sh
+++ post_build.sh
@@ -23,4 +23,4 @@ rm *.bat
 
 git add . --all
 git commit -m "Travis build"
-git push -f https://${GH_TOKEN}@github.com/giovannipessiva/l4w deploy
+git push -f https://${GH_TOKEN}@github.com/giovannipessiva/l4w deploy | sed -e 's/\/\/.*@github/\/\/[double secured]/g'
