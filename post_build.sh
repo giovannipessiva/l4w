13249e79a2bcbb17f5673d75708a30e67f88b084
giovannipessiva <rpt@altervista.org>
2020-03-23 00:04:11 +0100

    Test commit on deploy branch

--- post_build.sh
+++ post_build.sh
@@ -5,26 +5,25 @@ git branch deploy
 git checkout deploy
 
 rm .gitignore
-echo "/.vscode"  >> .gitignore
-echo "/data/database"  >> .gitignore
-echo "/src"  >> .gitignore
-echo ".env"  >> .gitignore
-echo ".travis.yml"  >> .gitignore
-echo "CONTRIBUTING.md"  >> .gitignore
-echo "Gruntfile.cjs"  >> .gitignore
-echo "LICENSE"  >> .gitignore
-echo "node_modules"  >> .gitignore
-echo "post_build.sh"  >> .gitignore
-echo "Procfile_inspect"  >> .gitignore
-echo "*.md"  >> .gitignore
-echo "*.bat"  >> .gitignore
-echo "tsconfig.json"  >> .gitignore
-echo "tslint.json"  >> .gitignore
-echo "webpack.config.cjs"  >> .gitignore
+echo "/.tscache" >> .gitignore
+echo "/.vscode" >> .gitignore
+echo "/data/database" >> .gitignore
+echo "/src" >> .gitignore
+echo ".env" >> .gitignore
+echo ".travis.yml" >> .gitignore
+echo "CONTRIBUTING.md" >> .gitignore
+echo "Gruntfile.cjs" >> .gitignore
+echo "LICENSE" >> .gitignore
+echo "node_modules" >> .gitignore
+echo "post_build.sh" >> .gitignore
+echo "Procfile_inspect" >> .gitignore
+echo "*.md" >> .gitignore
+echo "*.bat" >> .gitignore
+echo "tsconfig.json" >> .gitignore
+echo "tslint.json" >> .gitignore
+echo "webpack.config.cjs" >> .gitignore
 
-cat .gitignore
-
-git add . --all
+git add .
 git commit -m "Travis build"
 git push -f https://${GH_TOKEN}@github.com/giovannipessiva/l4w deploy > git.log 2>&1
 cat git.log | sed -e 's/\/\/.*@github/\/\/[double secured]/'
