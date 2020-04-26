e010ed9ee7766f6223deedc4c6e17ae58a766e67
giovannipessiva <rpt@altervista.org>
2020-04-26 19:52:54 +0200

    Login component

---
+++ src/client/components/Login.vue
@@ -0,0 +1,119 @@
+<template>
+    <div>
+        <div v-if="!logged">
+            <!-- Google login -->
+            <div id="login" class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
+            <!-- Facebook login -->
+            <div class="fb-login-button" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="false"
+                data-use-continue-as="false" data-width=""></div>
+            <br>
+            <slot name="whenLoggedOut"></slot>
+        </div>
+        <div v-else>
+            Welcome, {{ user }}!
+            <br>
+            <button v-on:click="logout">Logout</button>
+            <slot name="whenLoggedIn"></slot>
+        </div>
+    </div>
+</template>
+
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v6.0&appId=1885551381575204"></script>
+<script async defer src="https://apis.google.com/js/platform.js"></script>
+
+<script lang="ts">
+import Vue from "vue"
+let FB: any; // Loaded from Facebook script
+let gapi: any; // Loaded from Google script
+
+interface FBLoginResponse {
+    status: "connected" | "not_authorized" | "unknown",
+    authResponse?: {
+        accessToken: string,
+        expiresIn: any,
+        signedRequest: any,
+        userID: string
+    }
+}
+
+export default Vue.extend({
+    name: "login",
+    props: {
+        logged: {
+            type: Boolean,
+            required: false,
+            default: false
+        },
+        user: {
+            type: String,
+            required: false
+        }
+    },
+    created: function() {
+        // Add Google login meta tags
+        let head = document.getElementsByTagName("head")[0];
+        let meta;
+        
+        meta = document.createElement("meta");
+        meta.name = "google-signin-scope";
+        meta.content = "profile email";
+        head.appendChild(meta);
+
+        meta = document.createElement("meta");
+        meta.name = "google-signin-client_id";
+        meta.content = "106250700124-f3tm8cc2l6raccir6e5fi9osccuvhaj0.apps.googleusercontent.com";
+        head.appendChild(meta);
+
+        // Init Facebook login
+        let vueScope = this;
+        window["fbAsyncInit"] = function() {
+            FB.init({
+                appId: "1885551381575204",
+                autoLogAppEvents : true, // Enable cookies to allow the server to access the session
+                xfbml: true, // Parse social plugins on this webpage
+                version : "v6.0"
+            });
+
+            // Init Facebook login
+            FB.Event.subscribe("auth.statusChange", function(response: FBLoginResponse) {
+                vueScope.fbLoginStatusChangeCallback(response);
+            });
+        };
+    },
+    methods: {
+        logout: function logout() {
+            //TODO check which service is used, only logout from that service
+
+            if(this.logged) {
+                let vueScope = this;
+                // Facebook logout
+                FB.logout(function(response: any) {
+                    Vue.set(vueScope, "logged", false);
+                    location.reload();
+                });
+
+                // Google logout
+                let auth2 = gapi.auth2.getAuthInstance();
+                auth2.signOut().then(function () {
+                    Vue.set(vueScope, "logged", false);
+                    location.reload();
+                });
+            } else {
+                console.warn("Cannot logout, user is not currently logged in")
+            }
+        },
+        fbLoginStatusChangeCallback: function(response: FBLoginResponse) {
+            if(response.status === "connected") {
+                // L'utente ha effettuato l'accesso a Facebook e alla tua pagina Web.
+                console.log(response.authResponse);
+                
+                let vueScope = this;
+                FB.api("/me", function(responseMe: any) {
+                    console.log('Successful login for: ' + responseMe.name);
+                    Vue.set(vueScope, "user", responseMe.name);
+                });
+            } else console.log("Status: " + response.status)
+        }
+    }
+});
+</script>
--- src/client/tool/test/Tester.ts
+++ src/client/tool/test/Tester.ts
@@ -4,11 +4,27 @@ import { ClientUtils } from "../../core/util/Utils"
 import { MapManager } from "../../core/manager/MapManager"
 import { DataDefaults } from "../../../common/DataDefaults"
 
+import Vue from "vue"
+// @ts-ignore https://github.com/vuejs/vue-cli/issues/1198
+import LoginComponent from "../../components/Login.vue"
+
 export namespace Tester {
+
+    export function loadAuth() {
+        new Vue({
+            el: "#loginVue",
+            components: {
+                "login": LoginComponent,
+            },
+            data: {
+                logged: false,
+                user: undefined
+            }
+        });
+    }
     
     interface IPathfinderTestResult {
-        cell: ICell,
-    };
+        cell: ICell    };
 
     export function testPathfinding(width: number, height: number, eventI: number, eventJ: number, targetI: number,targetJ: number,blocks: number[]): IPathfinderTestResult[] {  
         // Initialize map
@@ -61,5 +77,123 @@ export namespace Tester {
             }
         } while(direction !== DirectionEnum.NONE);
         return result;
+    };
+
+    var stopMovement: boolean;
+    var blocks: number[];
+    const BLOCK = 15;
+    const NO_BLOCK = 0;
+
+    export function drawMap() {
+        let eventX = parseInt((<HTMLInputElement> document.getElementById("eventX")).value);
+        let eventY = parseInt((<HTMLInputElement> document.getElementById("eventY")).value);
+        let targetX = parseInt((<HTMLInputElement> document.getElementById("targetX")).value);
+        let targetY = parseInt((<HTMLInputElement> document.getElementById("targetY")).value);
+        let tableWidth = parseInt((<HTMLInputElement> document.getElementById("width")).value);
+        let tableHeight = parseInt((<HTMLInputElement> document.getElementById("height")).value);
+        
+        if(blocks === undefined || blocks.length !== tableWidth * tableHeight) {
+            blocks = new Array(tableWidth * tableHeight).fill(0);
+        }
+        
+        if(eventX >= tableWidth) {
+            eventX = tableWidth - 1;
+            (<HTMLInputElement> document.getElementById("eventX")).value = eventX + "";
+        }
+        if(targetX >= tableWidth) {
+            targetX = tableWidth - 1;
+            (<HTMLInputElement> document.getElementById("targetX")).value = targetX + "";
+        }
+        if(eventY >= tableHeight) {
+            eventY = tableHeight - 1;
+            (<HTMLInputElement> document.getElementById("eventY")).value = eventY + "";
+        }
+        if(targetY >= tableHeight) {
+            targetY = tableHeight - 1;
+            (<HTMLInputElement> document.getElementById("targetY")).value = targetY + "";
+        }
+        
+        let map = "";
+        for(let y=0; y<tableHeight; y++) {
+            for(let x=0; x<tableWidth; x++) {
+                let classes = "cell";
+                let gid = x + y * tableWidth;
+                if(targetX==x && targetY==y) {
+                    classes += " target";
+                }
+                if(blocks[gid] === BLOCK) {
+                    classes += " blocked";
+                }
+                if(eventX==x && eventY==y) {
+                    classes += " event";
+                }
+                map += "<div id='g"+ gid + "' class='"+classes+"' onclick='L4W_tester.Tester.toggleBlock(" + gid + ")'></div>";
+            }
+            map += "<br style='clear:both;'/>";
+        }
+        document.getElementById("map")!.innerHTML = map;
+    };
+
+    export function pathfinding(){
+        let eventX = parseInt((<HTMLInputElement> document.getElementById("eventX")).value);
+        let eventY = parseInt((<HTMLInputElement> document.getElementById("eventY")).value);
+        let targetX = parseInt((<HTMLInputElement> document.getElementById("targetX")).value);
+        let targetY = parseInt((<HTMLInputElement> document.getElementById("targetY")).value);
+        let tableWidth = parseInt((<HTMLInputElement> document.getElementById("width")).value);
+        let tableHeight = parseInt((<HTMLInputElement> document.getElementById("height")).value);
+        
+        let path = testPathfinding(tableWidth,tableHeight,eventX,eventY,targetX,targetY,blocks);
+
+        // Disable all input fields
+        (<HTMLInputElement> document.getElementById("eventX")).disabled=true;
+        (<HTMLInputElement> document.getElementById("eventY")).disabled=true;
+        (<HTMLInputElement> document.getElementById("targetX")).disabled=true;
+        (<HTMLInputElement> document.getElementById("targetY")).disabled=true;
+        (<HTMLInputElement> document.getElementById("width")).disabled=true;
+        (<HTMLInputElement> document.getElementById("height")).disabled=true;
+        (<HTMLInputElement> document.getElementById("find")).disabled=true;
+        (<HTMLInputElement> document.getElementById("stop")).disabled=false;
+        stopMovement = false;
+        
+        // Recursive function to simulate event movement along the path towards Target
+        function moveEvent(path: IPathfinderTestResult[], step: number) {
+            if(step < path.length && !stopMovement) {
+                // Move event by one step
+                (<HTMLInputElement> document.getElementById("eventX")).value = path[step].cell.i + "";
+                (<HTMLInputElement> document.getElementById("eventY")).value = path[step].cell.j + "";
+                
+                drawMap();
+                setTimeout(function() {
+                    moveEvent(path, step + 1);
+                },100);
+            } else {
+                // End of the path!
+                // Enable all input fields
+                (<HTMLInputElement> document.getElementById("eventX")).disabled=false;
+                (<HTMLInputElement> document.getElementById("eventY")).disabled=false;
+                (<HTMLInputElement> document.getElementById("targetX")).disabled=false;
+                (<HTMLInputElement> document.getElementById("targetY")).disabled=false;
+                (<HTMLInputElement> document.getElementById("width")).disabled=false;
+                (<HTMLInputElement> document.getElementById("height")).disabled=false;
+                (<HTMLInputElement> document.getElementById("find")).disabled=false;
+                (<HTMLInputElement> document.getElementById("stop")).disabled=true;
+            }
+        }
+        
+        document.getElementById("log")!.innerHTML = "";
+        moveEvent(path, 0);	
+    };
+
+    export function stop(){
+        stopMovement=true;
+    };
+
+    export function toggleBlock(gid: number) {
+        if(blocks[gid] === BLOCK) {
+            blocks[gid] = NO_BLOCK;
+        } else {
+            blocks[gid] = BLOCK;
+        }
+        drawMap();
     };
 }
